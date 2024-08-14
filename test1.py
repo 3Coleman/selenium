8b2a968430f8e53cbcf0ee81070d9e17b0110595
Abel Sibi <abelsibi05@gmail.com>
2024-08-14 20:16:02 +0530

    minute rearrangement done

--- test1.py
+++ test1.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
 driver=webdriver.Chrome()
 driver.get("https://www.python.org/")
+
 element=driver.find_element(By.NAME,"q")
 element.send_keys("selenium")
+
 button=driver.find_element(By.NAME,"submit")
 button.send_keys(Keys.RETURN)
 
