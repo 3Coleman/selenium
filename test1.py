340c0cb8cfa4b554aeef2bb5be5f07bee41c907d
Abel Sibi <abelsibi05@gmail.com>
2024-08-17 22:21:55 +0530

    modified test 1 file and make it as an test script

--- test1.py
+++ test1.py
@@ -1,20 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import unittest
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-driver=webdriver.Chrome()
-driver.get("https://www.python.org/")
+class PythonOrg(unittest.TestCase):
+    def setUp(self):
+        self.driver=webdriver.Chrome()
+    
+    def test_python(self):
+        driver=self.driver
+        driver.get("https://www.python.org/")
+        self.assertIn("Python",driver.title)
+        search=driver.find_element(By.NAME,"q")
+        search.send_keys("pycon")
+        but=driver.find_element(By.NAME,"submit")
+        but.send_keys(Keys.RETURN)
+        screenshot=driver.get_screenshot_as_file("python.png")
+        self.assertNotIn("No results found.",driver.page_source)
+        time.sleep(1)
 
-element=driver.find_element(By.NAME,"q")
-element.send_keys("selenium")
-
-button=driver.find_element(By.NAME,"submit")
-button.send_keys(Keys.RETURN)
-
-screenshot=driver.get_screenshot_as_file("page.png")
-
-driver.close
-
-
-hfhf
+        def tearDown(self):
+            self.driver.close()
+if __name__=="__main__":
+    unittest.main()
